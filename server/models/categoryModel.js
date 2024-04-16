9be5c6d13ab6eba58b9f90b497c0c66a816717c3
Sheff2000 <sheffpb@gmail.com>
2024-04-16 23:10:01 +0300

    add Category schema and model

---
+++ server/models/categoryModel.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+/***
+ * схема и функции для работы с коллецией - Категории часов
+ */
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    desc: String,
+    translit: {
+        type: String,
+        required: true,
+    },
+    imageUrl: String,
+});
+
+const categoryModel = mongoose.model('Category', categorySchema);
+
+// create new category
+const addCategory = async (data) => {
+    const category = new categoryModel(data);
+    return await category.save();
+};
+// get all category list
+const getAllCategory = async () => {
+    return await categoryModel.find();
+};
+// get one category by id
+const getCategoryById = async (id) => {
+    return await categoryModel.findById(id);
+};
+
+// update category info
+const updateCategory = async (id, data) => {
+    return await categoryModel.findByIdAndUpdate(id, data, { new: true });
+};
+
+// delete category
+const deleteCategory = async (id) => {
+    return await Category.findByIdAndDelete(id);
+};
+
+const CategoryWatch = {
+    addCategory,
+    getAllCategory,
+    getCategoryById,
+    updateCategory,
+    deleteCategory,
+};
+
+export default CategoryWatch;

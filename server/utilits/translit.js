9c4faa8589aa2eee9c5d80d04b4300a6c2ef414e
Sheff2000 <sheffpb@gmail.com>
2024-04-16 23:54:14 +0300

    add create translit func

---
+++ server/utilits/translit.js
@@ -0,0 +1,18 @@
+const generateTranslit = async (stroka) => {
+    const translitMap = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+        'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i',
+        'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+        'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+        'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+        'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': 'y', 'ь': '',
+        'э': 'e', 'ю': 'yu', 'я': 'ya',
+        ' ': '_', 
+    };
+
+    return stroka.split('').map(char => 
+        translitMap[char.toLowerCase()] || ''
+    ).join('');
+};
+
+export default generateTranslit;

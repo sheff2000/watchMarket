a9aeb400d0f976795ed76eed7edd5dcbf16638bc
SuperAkim <128703862+SuperAkim@users.noreply.github.com>
2024-04-17 13:17:10 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,39 @@
-// server/index.js
-const express = require('express');
+import express, { json } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+// файл настроек сервера
+import { configServer } from './include/serverConfig.js';
+// обработчик ошибок
+import { errorOperation } from './middleware/errorOperation.js';
+
+import watchRouter from './routes/watchRoutes.js';
+import categoryRouter from './routes/categoryRoutes.js';
+import brandRouter from './routes/brandRoutes.js';
+
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(json()); 
+app.use(express.urlencoded({ extended: true }));
+
+// Подключение маршрутов
 app.get('/', (req, res) => {
-  res.send('Server is running');
+  res.send('Да, работаю');
 });
+app.use('/api/category', categoryRouter);
+app.use('/api/brand', brandRouter);
+app.use('/api/watch', watchRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+app.use(errorOperation);
+
+const PORT = configServer.port;
+
+mongoose.connect(configServer.db_connect_string)
+.then(() =>{
+  console.log("MongoDB connected successfully")
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  
+})
+.catch(err => console.error("MongoDB connection error:", err));

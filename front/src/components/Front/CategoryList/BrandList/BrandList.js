a9153e558c43ab7a913395b5e467e7f0caf218d2
Sheff2000 <sheffpb@gmail.com>
2024-04-24 12:53:34 +0300

    up

--- front/src/components/Front/CategoryList/BrandList/BrandList.js
+++ front/src/components/Front/CategoryList/BrandList/BrandList.js
@@ -1,16 +1,20 @@
 import './BrandList.css';
 
 function BrandList({ brands, onClickBrand}) {
+    //brand-list-main-box
+    // brand-list-section
     return (
-        <div className='brand-list-main-box'>
-            <div className='brand-list-section'>
-                {brands.map((brand, index) => (
-                    <div key={index} className='brand-card sections-brands-card' onClick={() => onClickBrand(brand._id)}>   
-                        <img src={brand.imageUrl} alt={brand.name} />
-                    </div>
-                ))}
+       
+            <div className='container text-center'>
+                <div className='row brand-list-section'>
+                    {brands.map((brand, index) => (
+                        <div key={index} className='col-md-3 col-sm-4 brand-card sections-brands-card' onClick={() => onClickBrand(brand._id)}>   
+                            <img src={brand.imageUrl} alt={brand.name} />
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        
     );
 };
 
